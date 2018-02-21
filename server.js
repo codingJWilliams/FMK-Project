29671562c802cbd0b40550e2b6c2dcce4c254bd0
Jay Williams <course@jaywilliams.me>
2018-02-21 22:23:28 +0000

    Add some client side js etc

---
+++ server.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(8001);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/lobby.html");
+});
+app.get('/joingame', function(req, res) {
+    res.sendFile(__dirname + "/misc/joingame.html");
+});
+app.get("/startgame/1", (req, res) => {
+    res.sendFile(__dirname + "/misc/startgame.1.html");
+})
+io.on('connection', function(socket) {
+
+});
