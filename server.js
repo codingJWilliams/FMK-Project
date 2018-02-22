3e1dafe68df4ce42ec897958155157f6a489fb65
Jay Williams <course@jaywilliams.me>
2018-02-22 12:48:56 +0000

    Basic, insecure gameplay backend

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ var io = require('socket.io')(server);
 var db_u = require("./helpers/db_util");
 var gameSockets = {};
 var hostSockets = {};
-
+var peopleByGame = {}
 app.set('view engine', 'ejs');
-
 server.listen(8001);
-
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/lobby.html");
 });
@@ -21,7 +19,7 @@ app.get("/startgame/1", (req, res) => {
 app.get("/startgame/2/:people", async(req, res) => {
     var buf = Buffer.from(req.params.people, 'base64');
     var encJson = String(buf);
-    var id = await db_u.addGame(JSON.parse(encJson));
+    peopleByGame[id] = JSON.parse(encJson);
     gameSockets[id] = [];
 
     res.render("pages/host", {
@@ -32,5 +30,24 @@ io.on('connection', function(socket) {
     console.log("Got connection: " + socket.id)
     socket.on("host", function(gcode) {
         hostSockets[gcode] = socket;
+        setTimeout(() => { // Send player list every 750ms TODO: Events? only send player on join etc
+            socket.emit("playerRelist", gameSockets[gcode])
+        }, 750);
+    })
+    socket.on("join", function({ gcode, name }) {
+        gameSockets[gcode].push({ id: socket.id, name: name })
+        if (gameSockets[gcode].length > 2) return socket.emit("_err", "Too many players");
+        if (gameSockets[gcode].length == 1) {
+            // is masterplayer
+            socket.emit("u are master")
+            socket.emit("people", peopleByGame[gcode]);
+            socket.on("choice", function(choices) {
+                gameSockets[gcode].forEach(a => {
+                    io.to(a.id).emit("masterChoice", choices);
+                })
+            })
+        } else if (gameSockets[gcode].length == 2) {}
+        // is slaveplayer
+        socket.emit("u are slave")
     })
 });
